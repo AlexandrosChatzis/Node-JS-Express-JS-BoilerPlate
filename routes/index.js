b39392b78a77f547bda64cc27aa92b92ec685d21
alex <Alexandros_Chatzis@yahoo.com>
2020-02-07 14:38:28 +0200

    mail and cronJob

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,44 @@ const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 const { redirectAuth } = require("../config/redirectAuth");
 const User = require("../models/User");
+const ejs = require("ejs");
+var fs = require("fs");
+//mail library
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILHOST,
+  port: process.env.MAILPORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAILUSR, // generated ethereal user
+    pass: process.env.MAILPW // generated ethereal password
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false
+  }
+});
+
 //Encrypt Password
 const bcrypt = require("bcryptjs");
 router.get("/", redirectAuth, (req, res) => {
   res.render("welcome");
 });
+router.get("/cron", ensureAuthenticated, (req, res) => {
+  var CronJob = require("cron").CronJob;
+  var job = new CronJob(
+    "* * * * * *",
+    () => {
+      console.log("You will see this message every second");
+    },
+    null,
+    true,
+    "America/Los_Angeles"
+  );
+  job.start();
 
+  res.redirect("dashboard");
+});
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
   req.session.name = req.user.name;
   res.locals.name = req.user.name;
@@ -63,4 +95,27 @@ router.post("/change_settings", ensureAuthenticated, async (req, res) => {
   //   user: req.user
   // });
 });
+
+router.get("/mail", ensureAuthenticated, async (req, res) => {
+  const data = await ejs.renderFile(__dirname + "/../views/mail.ejs", {
+    name: "test"
+  });
+
+  const mailOptions = {
+    from: process.env.MAILUSR,
+    to: process.env.MAILUSR,
+    subject: "Sending Email using Node.js",
+    html: data
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  req.flash("success_msg", "mail sent");
+  res.redirect("dashboard");
+});
+
 module.exports = router;
